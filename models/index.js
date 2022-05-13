051c0d99f9d0f8efc282e739a5d2f4c779a05fb1
Sebastien Yalicheff <sebastien.yalicheff@autoequip.local>
2022-05-13 12:26:35 +0200

    first Push

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const db = {};
+db.mongoose = mongoose;
+db.user = require("./user.model");
+db.role = require("./role.model");
+module.exports = db;
---
+++ server.js
@@ -0,0 +1,113 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const express = require("express")
+const app = express()
+const bcrypt = require("bcrypt")
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+// BDD
+const db = require("./models");
+
+
+
+
+const initializePassport = require("./passport-config")
+
+db.mongoose
+    .connect('mongodb+srv://App_Autoequip:Autoequip94@cluster0.kjbyh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Successfully connect to MongoDB.");
+    })
+    .catch(err => {
+        console.error("Connection error", err);
+        process.exit();
+    });
+async({
+    await getUsers(db).then((users) => {
+        console.log(users)
+        initializePassport(
+            passport,
+            email => users.find(user => user.email === email),
+            id => users.find(user => user.id === id)
+        )
+    })
+})
+
+
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+app.get('/', checkAuthenticated, async(req, res) => {
+    const users = await getUsers(User)
+    res.render('index.ejs', { name: req.user.name })
+  })
+  
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+})
+  
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+  
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+})
+  
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+      })
+      res.redirect('/login')
+    } catch {
+      res.redirect('/register')
+    }
+})
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+})
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+  
+    res.redirect('/login')
+}
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/')
+    }
+    next()
+}
+async function getUsers(db){
+    db.user.find({},'id email').then(function (users) {
+        console.log(users)
+       return(users);
+    });  
+}
+app.listen(8080)
