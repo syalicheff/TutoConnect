53985787827e661e5ae48b3ed5a2e2c53cc374fb
syalicheff <yalicheff.sebastien@gmail.com>
2022-05-16 14:50:41 +0200

    Fin roles et users

--- passport-config.js
+++ passport-config.js
@@ -4,12 +4,14 @@ const bcrypt = require('bcrypt')
 
 function initialize(passport, getUserByEmail, getUserById) {
   const authenticateUser = async (email, password, done) => {
+    
     const user = getUserByEmail(email)
+    console.log(user)
     if (user == null) {
       return done(null, false, { message: 'No user with that email' })
     }
-
     try {
+      console.log(password,user.password)
       if (await bcrypt.compare(password, user.password)) {
         return done(null, user)
       } else {
--- server.js
+++ server.js
@@ -12,9 +12,6 @@ const methodOverride = require('method-override')
 // BDD
 const db = require("./models");
 
-
-
-
 const initializePassport = require("./passport-config")
 
 db.mongoose
@@ -29,73 +26,81 @@ db.mongoose
         console.error("Connection error", err);
         process.exit();
     });
-async({
-    await getUsers(db).then((users) => {
-        console.log(users)
+    getUsers(db).then((users) => {
         initializePassport(
             passport,
             email => users.find(user => user.email === email),
             id => users.find(user => user.id === id)
         )
+        //console.log(users)
     })
-})
 
+    app.set('view-engine', 'ejs')
+    app.use(express.urlencoded({extended: true}))
+    app.use(flash())
+    app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+    }))
+    app.use(passport.initialize())
+    app.use(passport.session())
+    app.use(methodOverride('_method'))
+    const checkRoles = (text) => {
+        return (req, res, next) => {
+          if(req.user.roles.includes(text)){
+                next();
+            }
+            else{
+                res.send("Vous ne possédez pas les permissions nécéssaires")
+            } 
+        };
+      };
+    app.get('/', checkAuthenticated , checkRoles('admin'), async(req, res) => {
 
+        res.render('index.ejs', { name: req.user.username })
+    })
+    
+    app.get('/login', checkNotAuthenticated, (req, res) => {
+        res.render('login.ejs')
+    })
+    
+    app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login',
+        failureFlash: true
+    }))
+    
+    app.get('/register', checkNotAuthenticated, (req, res) => {
+        res.render('register.ejs')
+    })
+    app.get('/test', checkAuthenticated , checkRoles('admin'), (req, res) => {
+        //res.render('register.ejs')
+    })
+    
+    app.post('/register', checkNotAuthenticated, async (req, res) => {
+        try {
+            const hashedPassword = await bcrypt.hash(req.body.password, 10)
+            var newUser = new db.user({ username: req.body.username, email: req.body.email,  password: hashedPassword ,roles: req.body.roles})
+            newUser.save(function (err, user) {
+                if (err) return console.error(err);
+                console.log(user.username + " a été crée");
+              });
+            res.redirect('/login')
+        } catch (err){
+            console.log(err)
+            res.redirect('/register')
+        }
+    })
+    app.delete('/logout', (req, res) => {
+        req.logOut()
+        res.redirect('/login')
+    })
 
-app.set('view-engine', 'ejs')
-app.use(express.urlencoded({extended: false}))
-app.use(flash())
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(methodOverride('_method'))
-
-app.get('/', checkAuthenticated, async(req, res) => {
-    const users = await getUsers(User)
-    res.render('index.ejs', { name: req.user.name })
-  })
-  
-app.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render('login.ejs')
-})
-  
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-}))
-  
-app.get('/register', checkNotAuthenticated, (req, res) => {
-    res.render('register.ejs')
-})
-  
-app.post('/register', checkNotAuthenticated, async (req, res) => {
-    try {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10)
-      users.push({
-        id: Date.now().toString(),
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword
-      })
-      res.redirect('/login')
-    } catch {
-      res.redirect('/register')
-    }
-})
-app.delete('/logout', (req, res) => {
-    req.logOut()
-    res.redirect('/login')
-})
 function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
       return next()
     }
-  
     res.redirect('/login')
 }
 function checkNotAuthenticated(req, res, next) {
@@ -104,10 +109,12 @@ function checkNotAuthenticated(req, res, next) {
     }
     next()
 }
+
 async function getUsers(db){
-    db.user.find({},'id email').then(function (users) {
-        console.log(users)
-       return(users);
-    });  
+    return new Promise(function(resolve, reject) {
+        db.user.find({},'id email password username roles').then(function (users) {
+            resolve(users)   
+        });  
+    })
 }
 app.listen(8080)
